901660f9d54e08d3c2c01db129404113d220871b
Moritz Manner <moritz.manner@student.hpi.de>
2017-03-01 23:25:56 +0100

    Fixed tests for #1

--- spec/views/authors_controller/new.html.erb_spec.rb
+++ spec/views/authors_controller/new.html.erb_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "authors_controller/new.html.erb", type: :view do
-  render
-  describe "new" do
-    it "renders the page" do
-      get page
-    end
-  end
+
 end
